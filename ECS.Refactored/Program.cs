94b47e3a582524c6f4b3f94c7d944ba5c27147e9
ThomasStenholtLaursen <201905236@post.au.dk>
2022-02-16 12:18:50 +0100

    Quick code clean-up

--- ECS.Refactored/Program.cs
+++ ECS.Refactored/Program.cs
@@ -7,9 +7,10 @@ namespace ECS.Refactored
         static void Main(string[] args)
         {
             Console.WriteLine("Testing ECS.Refactored");
-
+            var sensor = new TempSensor();
+            var regulator = new Heater();
             // Make an ECS with a threshold of 23
-            var control = new ECS(23);
+            var control = new ECS(23, sensor, regulator);
 
             for (int i = 1; i <= 15; i++)
             {
